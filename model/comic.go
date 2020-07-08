c1d5ce9f7ef246516ca661a5c97be7a13f81b0fb
Seif Wu <seif.wuxf@gmail.com>
2020-07-08 23:00:20 +0800

    feat: add comic model

---
+++ model/comic.go
@@ -0,0 +1,13 @@
+package model
+
+import "github.com/jinzhu/gorm"
+
+// Comic 漫画模型
+type Comic struct {
+	gorm.Model
+
+	Title    string `json:"title" gorm:"type:varchar(20); not null; comment:'漫画名称'"`
+	Describe string `json:"describe" gorm:"comment:'描述'"`
+	Author   string `json:"author" gorm:"comment:'作者'"`
+	Cover    string `json:"cover" gorm:"comment:'封面'"`
+}
